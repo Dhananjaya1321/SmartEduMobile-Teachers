430ca0c4d9d40909722b71d17303709a0b2b2170
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-08-18 15:40:31 +0530

    MarkAttendanceScreen was completed and API integrated

---
+++ controllers/AttendanceController.ts
@@ -0,0 +1,27 @@
+import apiClient from "@/apiClient";
+
+const attendanceAPIController = {
+    saveAttendance: async (payload: any) => {
+        try {
+            const response = await apiClient.post(`/attendance`, payload);
+            if (response.status === 200 && response.data.state === "OK") {
+                return response.data.data;
+            }
+        } catch (error) {
+            return null;
+        }
+    },
+    getAllAttendanceByClassId: async (classId:any) => {
+        try {
+            const response = await apiClient.get(`/attendance/class/${classId}`);
+            if (response.status === 200 && response.data.state === "OK") {
+                return response.data;
+            }
+            return null;
+        } catch (error) {
+            return null;
+        }
+    },
+};
+
+export default attendanceAPIController;
