c0d9782050bb9e5b09c615bbc2b6cb0a6d30043b
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2025-08-18 11:25:54 +0530

    findMyClassesTimetableToTeacher API integrated

---
+++ controllers/ClassTimetablesController.ts
@@ -0,0 +1,28 @@
+import apiClient from "@/apiClient";
+
+const classTimetablesAPIController = {
+    findMyClassesTimetableToTeacher: async () => {
+        try {
+            const response = await apiClient.get(`/timetables/my-classes/to-teacher`);
+            if (response.status === 200 && response.data.state === "OK") {
+                return response.data.data;
+            }
+            return null;
+        } catch (error) {
+            return null;
+        }
+    },
+    findOtherClassesTimetableToTeacherByClassId: async (classId:any) => {
+        try {
+            const response = await apiClient.get(`/timetables/other-classes/to-teacher/{classId}`);
+            if (response.status === 200 && response.data.state === "OK") {
+                return response.data.data;
+            }
+            return null;
+        } catch (error) {
+            return null;
+        }
+    },
+};
+
+export default classTimetablesAPIController;
